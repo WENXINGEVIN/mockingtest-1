02a19b60f92f5e31eb31e265300c32b628035126
Xiaofeng Li <xiaofeng@wolfram.com>
2016-09-21 16:00:00 -0500

    update my examples

---
+++ JavascriptExamples/WebContent/RequireJS/requirejsAndJQuery/config.js
@@ -0,0 +1,9 @@
+requirejs.config({
+	baseUrl: "lib",
+	paths: {
+		jquery: ['https://code.jquery.com/jquery-3.1.0.min','jquery-2.0.0.min'],
+	    jqueryui:'',
+		angular:'',
+	    backbone:''
+	}
+});
---
+++ JavascriptExamples/WebContent/RequireJS/requirejsAndJQuery/lib/template/jqueryfile.js
@@ -0,0 +1,7 @@
+define(['jquery'], function($) {
+ 	return { 
+    	 showme: function() {
+    		 	$("body").html("testme if you can");
+              }
+    }
+});
