393ea41d590cd5906368bd1124b3b24195665133
xiaofeng li <xlics05@gmail.com>
2016-09-21 21:03:09 -0500

    remove a line

--- app/src/main/java/com/webbertech/web/security/SecurityWebApplicationInitializer.java
+++ app/src/main/java/com/webbertech/web/security/SecurityWebApplicationInitializer.java
@@ -3,5 +3,4 @@ package com.webbertech.web.security;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
-
 }
